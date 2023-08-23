5bf98ffdf0188c04b805136e60ce9a4974c1f2d5
abhiram <81793953+modkavartini@users.noreply.github.com>
2023-08-24 03:16:02 +0530

    used name instead of tag_name

--- @resources/update.ps1
+++ @resources/update.ps1
@@ -1,14 +1,15 @@
 function update {
-    stop-Process -name "Rainmeter"
     $API=invoke-WebRequest -uri "https://api.github.com/repos/modkavartini/lazydev/releases/latest" -useBasicParsing
     $URL=($API | convertFrom-Json).assets.browser_download_url
-    $lV=($API | convertFrom-Json).tag_name
+    $lV=($API | convertFrom-Json).name
     $outPath="C:/Windows/Temp/lazy_zzz.rmskin"
     write-Host "> downloading lazydev $lV..." -foregroundColor red
     $WC=new-Object System.Net.WebClient
     $WC.downloadFile($URL, $outPath)
     write-Host "> installing lazydev $lV..." -foregroundColor green
     start-Sleep 1
+    $shell=new-Object -ComObject Shell.Application
+    $shell.minimizeAll()
     start-Process $outPath
     if($null -notmatch (get-Process "SkinInstaller" -ea 0)) {
         start-Sleep 2
